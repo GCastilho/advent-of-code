dd923711e1e7ff5e2e6a6873884eb76466505687
GCastilho <castilho.gabriel14@gmail.com>
2022-12-03 22:23:07 -0300

    part 1

--- 2022/day-03/src/main.rs
+++ 2022/day-03/src/main.rs
@@ -1,3 +1,42 @@
+use std::{collections::HashMap, fs};
+
+fn init_priority() -> HashMap<char, usize> {
+    let mut priority = HashMap::new();
+
+    ('a'..='z')
+        .enumerate()
+        .for_each(|(i, v)| {
+            priority.insert(v, i + 1);
+        });
+
+    ('A'..='Z')
+        .enumerate()
+        .for_each(|(i, v)| {
+            priority.insert(v, i + 27);
+        });
+
+    priority
+}
+
 fn main() {
-    println!("Hello, world!");
+    let prority_map = init_priority();
+
+    let input = fs::read_to_string("input.txt").unwrap();
+
+    let priority_sum = input
+        .split_terminator("\n")
+        .map(|line| line.chars().collect::<Vec<char>>())
+        .map(|char_vec| {
+            let middle = char_vec.len() / 2;
+            let (first, second) = (&char_vec[..middle], &char_vec[middle..]);
+
+            let duplicated = second.iter()
+                .find(|&item| first.contains(item))
+                .expect("Expected first half to contain an item from the second half");
+            duplicated.clone()
+        })
+        .filter_map(|duplicated| prority_map.get(&duplicated))
+        .sum::<usize>();
+
+    println!("Priority sum: {}", priority_sum);
 }
