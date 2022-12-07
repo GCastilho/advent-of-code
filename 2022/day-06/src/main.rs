23c40115868b704d654d1965f4040d17ae094352
G@briel Castilho <gabriel_castilho@hst.br>
2022-12-07 15:44:29 -0300

    part 1

--- 2022/day-06/src/main.rs
+++ 2022/day-06/src/main.rs
@@ -1,3 +1,25 @@
+use std::{fs, collections::HashSet};
+use circular_queue::CircularQueue;
+
+// const FILENAME: &str = "example.txt";
+const FILENAME: &str = "input.txt";
+
 fn main() {
-    println!("Hello, world!");
+    let input = fs::read_to_string(FILENAME).unwrap();
+
+    let mut queue = CircularQueue::with_capacity(4);
+    let mut total_amount = 0;
+
+    for char in input.chars().take_while(|char| *char != '\n') {
+        queue.push(char);
+        total_amount += 1;
+
+        let mut uniq = HashSet::new();
+        queue.iter().for_each(|c| {
+            uniq.insert(c);
+        });
+        if uniq.len() == 4 { break; }
+    }
+
+    println!("counter: {}", total_amount);
 }
