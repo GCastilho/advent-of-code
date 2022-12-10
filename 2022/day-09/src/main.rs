c67ecce7a82d6c80fab56bb22f53fba1e3c857e4
GCastilho <castilho.gabriel14@gmail.com>
2022-12-10 13:54:36 -0300

    part 2

--- 2022/day-09/src/main.rs
+++ 2022/day-09/src/main.rs
@@ -11,18 +11,43 @@ enum Direction {
     Right,
 }
 
-fn is_same(p1: (i32, i32), p2: (i32, i32)) -> bool {
+fn is_same(p1: &(i32, i32), p2: &(i32, i32)) -> bool {
     let (x1, y1) = p1;
     let (x2, y2) = p2;
     x1 == x2 && y1 == y2
 }
 
-fn is_adjacent(p1: (i32, i32), p2: (i32, i32)) -> bool {
+fn is_adjacent(p1: &(i32, i32), p2: &(i32, i32)) -> bool {
     let (x1, y1) = p1;
     let (x2, y2) = p2;
     let dx = (x1 - x2).abs();
     let dy = (y1 - y2).abs();
     dx + dy == 1 || dx > 0 && dy > 0 && dx + dy == 2
+}
+
+fn should_move(p1: &(i32, i32), p2: &(i32, i32)) -> bool {
+    !is_same(p1, p2) && !is_adjacent(p1, p2)
+}
+
+fn move_knot(head: &(i32, i32), tail: &mut (i32, i32)) {
+    let (hx, hy) = *head;
+    let (tx, ty) = *tail;
+
+    let x_diff = hx - tx;
+    let y_diff = hy - ty;
+
+    if (hx - tx).abs() > 1 || (hy - ty).abs() > 1 {
+        if x_diff > 0 {
+            tail.0 += 1;
+        } else if x_diff < 0 {
+            tail.0 -= 1;
+        }
+        if y_diff > 0 {
+            tail.1 += 1;
+        } else if y_diff < 0 {
+            tail.1 -= 1;
+        }
+    }
 }
 
 fn main() {
@@ -44,30 +69,40 @@ fn main() {
         })
         .collect::<Vec<_>>();
 
-    let mut head = (0, 0);
-    let mut tail = (0, 0);
-    let mut places = vec![(0, 0)];
+    let mut knots = vec![];
+    let nodes = 10;
+    for _ in 0..nodes {
+        knots.push((0, 0));
+    }
+
+    let mut uniques = HashSet::new();
+    uniques.insert((0, 0));
 
     for (direction, steps) in motions.iter() {
         for _ in 0..*steps {
-            let old_head = head.clone();
+            let mut head = knots.first_mut().unwrap();
             match direction {
                 Direction::Up => head.0 += 1,
                 Direction::Down => head.0 -= 1,
                 Direction::Left => head.1 -= 1,
                 Direction::Right => head.1 += 1,
             };
-            if is_same(head, tail) || is_adjacent(head, tail){
-                continue;
-            }
-            tail = old_head;
-            places.push(tail);
-        }
-    }
 
-    let mut uniques = HashSet::new();
-    for item in places.iter() {
-        uniques.insert(item);
+            let mut current_head = head.clone();
+
+            let iter = knots.iter_mut().skip(1);
+            for knot in iter {
+                if should_move(&current_head, knot) {
+                    move_knot(&current_head, knot);
+                }
+                current_head = knot.clone();
+            }
+
+            // println!("{:?}", knots);
+
+            let tail = knots.last().unwrap();
+            uniques.insert(*tail);
+        }
     }
 
     println!("uniques: {}", uniques.len());
