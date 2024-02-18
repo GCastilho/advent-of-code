76f06e94be3ab3e7710253e11513f226ca7267a4
GCastilho <castilho.gabriel14@gmail.com>
2024-02-18 16:52:00 -0300

    2023/10 Part two

--- 2023/day-10/src/main.rs
+++ 2023/day-10/src/main.rs
@@ -16,6 +16,11 @@ fn main() {
     println!(
         "Part one, it takes {steps_to_farthest_point} to get to the farthest point from the starting position"
     );
+
+    println!(
+        "Part two, there are {} tiles enclosed by the main loop",
+        sketch.get_main_loop().get_tiles_enclosed_by_main_loop()
+    );
 }
 
 #[derive(Debug, EnumIter)]
@@ -82,6 +87,10 @@ impl Pipe {
             _ => false,
         }
     }
+
+    fn is_inversion(&self) -> bool {
+        matches!(&self, Pipe::Vertical | Pipe::NorthWest | Pipe::NorthEast)
+    }
 }
 
 impl Element for Pipe {
@@ -142,7 +151,7 @@ impl Sketch {
         let start_pos = self.start.unwrap();
 
         let mut longest_distance = 0;
-        let mut m_distances = Compressed::zero((self.matrix.columns, self.matrix.rows));
+        let mut m_distances = Compressed::zero((self.matrix.rows, self.matrix.columns));
         m_distances.set(start_pos, Distance(Some(0)));
 
         let mut queue = VecDeque::new();
@@ -186,6 +195,99 @@ impl Sketch {
 
         longest_distance as usize
     }
+
+    fn get_start_pipe(connected_sides: [Side; 2]) -> Pipe {
+        match connected_sides {
+            [Side::Up, Side::Down] => Pipe::Vertical,
+            [Side::Up, Side::Right] => Pipe::NorthEast,
+            [Side::Up, Side::Left] => Pipe::NorthWest,
+
+            [Side::Right, Side::Down] => Pipe::SouthEast,
+            [Side::Right, Side::Left] => Pipe::Horizontal,
+            [Side::Right, Side::Up] => Pipe::NorthEast,
+
+            [Side::Down, Side::Left] => Pipe::SouthWest,
+            [Side::Down, Side::Up] => Pipe::Vertical,
+            [Side::Down, Side::Right] => Pipe::SouthEast,
+
+            [Side::Left, Side::Up] => Pipe::NorthWest,
+            [Side::Left, Side::Right] => Pipe::Horizontal,
+            [Side::Left, Side::Down] => Pipe::SouthWest,
+
+            side => panic!("Não tem como conectar Start com essa combinação: {side:?}"),
+        }
+    }
+
+    fn get_main_loop(&self) -> Sketch {
+        let start_pos = self.start.unwrap();
+        let mut main_loop_matrix = Conventional::zero((self.matrix.rows, self.matrix.columns));
+        *main_loop_matrix.index_mut(start_pos) = Pipe::Start;
+
+        let mut queue = VecDeque::new();
+        let mut connected_sides = Vec::with_capacity(2);
+
+        // Preenche os que conectam com start
+        for side in Side::iter() {
+            let Some(pos) = side.get_pos(start_pos) else {
+                continue;
+            };
+            let item = self.matrix.index(pos);
+            if item.connects_from(&side) {
+                queue.push_back(pos);
+                *main_loop_matrix.index_mut(pos) = *self.matrix.index(pos);
+                connected_sides.push(side)
+            }
+        }
+
+        let start_pipe = Self::get_start_pipe(
+            connected_sides
+                .try_into()
+                .expect("Start pipe deveria ter EXATAMENTE 2 lados conectados"),
+        );
+        *main_loop_matrix.index_mut(start_pos) = start_pipe;
+
+        while let Some(pos) = queue.pop_front() {
+            let item = self.matrix.index(pos);
+            // Assumindo que não tem dead ends e que sempre os pipes conectam
+            let sides = item.get_sides();
+            let sides = sides.expect("Não implementado dead end");
+            for side in sides {
+                let Some(pos) = side.get_pos(pos) else {
+                    continue;
+                };
+                if matches!(main_loop_matrix.index(pos), Pipe::Ground) {
+                    *main_loop_matrix.index_mut(pos) = *self.matrix.index(pos);
+                    queue.push_back(pos);
+                }
+            }
+        }
+
+        Sketch {
+            matrix: main_loop_matrix,
+            start: self.start,
+        }
+    }
+
+    fn get_tiles_enclosed_by_main_loop(&self) -> usize {
+        let mut tiles_enclosed_by_main_loop = 0;
+
+        for y in 0..self.matrix.rows() {
+            for x in 0..self.matrix.columns() {
+                if matches!(self.matrix.index((y, x)), Pipe::Ground) {
+                    let inversions = (0..x)
+                        .map(|column| self.matrix.index((y, column)).is_inversion())
+                        .filter(|v| *v)
+                        .count();
+
+                    if inversions % 2 == 1 {
+                        tiles_enclosed_by_main_loop += 1;
+                    }
+                }
+            }
+        }
+
+        tiles_enclosed_by_main_loop
+    }
 }
 
 impl fmt::Display for Sketch {
@@ -228,5 +330,29 @@ mod test {
     fn steps_to_farthest_point_complex() {
         let sketch = Sketch::from("./examples/complex.txt");
         assert_eq!(sketch.get_steps_to_farthest_point(), 8)
+    }
+
+    #[test]
+    fn tiles_enclosed_by_main_loop() {
+        let sketch = Sketch::from("./examples/inside_loop_1.txt");
+        assert_eq!(sketch.get_main_loop().get_tiles_enclosed_by_main_loop(), 4);
+    }
+
+    #[test]
+    fn tiles_enclosed_by_main_loop_2() {
+        let sketch = Sketch::from("./examples/inside_loop_2.txt");
+        assert_eq!(sketch.get_main_loop().get_tiles_enclosed_by_main_loop(), 4);
+    }
+
+    #[test]
+    fn tiles_enclosed_by_main_loop_3() {
+        let sketch = Sketch::from("./examples/inside_loop_3.txt");
+        assert_eq!(sketch.get_main_loop().get_tiles_enclosed_by_main_loop(), 8);
+    }
+
+    #[test]
+    fn tiles_enclosed_by_main_loop_4() {
+        let sketch = Sketch::from("./examples/inside_loop_4.txt");
+        assert_eq!(sketch.get_main_loop().get_tiles_enclosed_by_main_loop(), 10);
     }
 }
